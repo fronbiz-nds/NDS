a2bc8c82170b2e7924b3ff5e2e4b34fc1ec2c683
sylee <linggo01@naver.com>
2026-02-18 13:57:10 +0900

    refactor: 컴퍼넌트 로드방식 변경

--- content/worklist/assets/js/guide.js
+++ content/worklist/assets/js/guide.js
@@ -1,168 +1,90 @@
-
 function buildGuide() {
-    /* GUIDE LIST */
-    document.querySelectorAll('.gl script').forEach(function(script, i, scripts) {
-        var item = document.createElement('div');
-        var ex = document.createElement('div');
-        var html = window['item'+ i] = script.textContent.replace('<!-- [코드시작] -->', '').replace('<!-- [코드끝] -->', '').trim();
-        var size = !script.dataset.size ? 1 : script.dataset.size;
+    document.querySelectorAll('.gl script').forEach(function(script, i) {
 
-        // 아이템
+        var item = document.createElement('div');
+        var preview = document.createElement('div');
+        var etc = document.createElement('div');
+
+        var html = script.textContent.trim();
+
+        /* 기본 세팅 */
         item.classList.value = script.classList.value;
         item.classList.add('gl__item');
-        item.classList.add('-x'+ size);
         item.dataset.title = script.dataset.title;
-        if (script.dataset.desc) { item.dataset.desc = script.dataset.desc; }
-        if (script.dataset.notes) { item.dataset.notes = script.dataset.notes; }
 
-        // 프리뷰
-        ex.classList.add('gl__preview');
-        ex.innerHTML = html;
-        ex.addEventListener('click', function(e) {
-            var target = e.target;
-            navigator.clipboard.writeText(html).then(function() {
+        if (script.dataset.desc) item.dataset.desc = script.dataset.desc;
+        if (script.dataset.notes) item.dataset.notes = script.dataset.notes;
 
-            });
+        /* preview */
+        preview.classList.add('gl__preview');
+        preview.innerHTML = html;
+
+        /* etc 영역 */
+        etc.classList.add('gl__etc');
+
+        /* 설명 */
+        var desc = document.createElement('span');
+        desc.classList.add('gl__desc');
+        desc.innerHTML = script.dataset.title ? script.dataset.title : '';
+
+        /* 소스보기 버튼 */
+        var detailBtn = document.createElement('button');
+        detailBtn.type = 'button';
+        detailBtn.classList.add('gl__detail');
+        detailBtn.innerHTML = '<span class="hide">소스보기</span>';
+        detailBtn.addEventListener('click', function () {
+
+            if (document.querySelector('.gd')) {
+                document.querySelector('.gd').remove();
+            }
+
+            var gd = document.createElement('div');
+            gd.classList.add('gd');
+
+            var close = document.createElement('button');
+            close.type = 'button';
+            close.classList.add('gd__close');
+            close.innerHTML = '<span class="hide">닫기</span>';
+
+            var title = document.createElement('div');
+            title.classList.add('gd__title');
+            title.innerText = item.dataset.title;
+
+            var dev = document.createElement('div');
+            dev.classList.add('gd__dev');
+
+            var code = document.createElement('pre');
+            code.classList.add('gd__code');
+
+            var source = item.querySelector('.gl__preview').innerHTML.trim();
+            code.textContent = source;
+
+            dev.append(code);
+            gd.append(close, title, dev);
+            document.body.append(gd);
+
+            item.classList.add('-active');
+            gd.classList.add('-active');
         });
-        item.append(ex);
+
+        /* 카피하기 버튼 */
+        var copyBtn = document.createElement('button');
+        copyBtn.type = 'button';
+        copyBtn.classList.add('gl__copy');
+        copyBtn.innerHTML = '<span class="hide">카피하기</span>';
+
+        copyBtn.addEventListener('click', function () {
+            navigator.clipboard.writeText(html);
+        });
+
+        /* 조립 */
+        etc.append(desc, detailBtn, copyBtn);
+        item.append(preview, etc);
 
         script.insertAdjacentElement('afterend', item);
         script.remove();
     });
 }
-
-document.querySelectorAll('.gl__item').forEach(function(item, i) {
-    item.addEventListener('mouseenter', function(e) {
-        var target = e.target;
-        var items = document.querySelectorAll('.gl__item');
-        var testButton = document.createElement('button');
-        var detailButton = document.createElement('button');
-
-        // testButton.type = 'button';
-        // testButton.classList.add('gl__test');
-        // testButton.innerHTML = '<span>테스트</span>';
-        // testButton.addEventListener('click', function(e) {
-        //     var cTarget = e.currentTarget;
-        //     var item = cTarget.parentNode;
-
-        //     item.classList.toggle('test-mode');
-        // });
-
-        // target.append(testButton);
-
-        // 코드보기
-        detailButton.type = 'button';
-        detailButton.classList.add('gl__detail');
-        detailButton.innerHTML = '<span>상세</span>';
-        detailButton.addEventListener('click', function(e) {
-            var target = e.target;
-            var item = target.parentNode;
-            var gd = document.createElement('div');
-            var title = document.createElement('div');
-            var dev = document.createElement('div');
-            var code = document.createElement('pre');
-            var info = document.createElement('div');
-            var close = document.createElement('button');
-            var desc = document.createElement('div');
-            var subTitle = document.createElement('div');
-            var notes = document.createElement('div');
-
-            if (document.querySelector('.gd')) { document.querySelector('.gd').remove(); };
-
-            gd.classList.add('gd');
-
-            title.classList.add('gd__title');
-            title.innerText = item.dataset.title;
-
-            gd.append(title);
-
-            code.classList.add('gd__code');
-            code.textContent = window['item'+ i];
-
-            dev.classList.add('gd__dev');
-            dev.append(code);
-
-            gd.append(dev);
-
-            info.classList.add('gd__info');
-
-            if (item.dataset.desc) {
-                desc.classList.add('gd__desc');
-                desc.innerHTML = item.dataset.desc;
-
-                info.append(desc);
-            }
-
-            if (item.dataset.notes) {
-                subTitle.classList.add('gd__sub-title');
-                subTitle.innerText = '수정내역';
-
-                info.append(subTitle);
-
-                notes.classList.add('gd__notes');
-
-                item.dataset.notes.replaceAll(/[\{\}/\s/\n]/g, '').split(',').forEach(function(item, i) {
-                    var result = item.split(':');
-                    var note = document.createElement('div');
-
-                    note.classList.add('gd__note');
-                    note.dataset.date = result[0];
-                    note.innerHTML = result[1];
-
-                    notes.append(note);
-                });
-
-                info.append(notes);
-            }
-
-            gd.append(info);
-
-            close.type = 'button';
-            close.classList.add('gd__close');
-            close.innerHTML = '<span class="hide">닫기</span>';
-
-            gd.append(close);
-
-            document.querySelector('body').append(gd);
-
-            if (item.dataset.desc) { desc = item.dataset.desc; }
-
-            items.forEach(function(item) { item.classList.remove('-active') });
-            item.classList.add('-active');
-
-            document.querySelector('.gd').classList.add('-active');
-
-            if (item.dataset.notes) {
-                notes = item.dataset.notes;
-            }
-
-            if (desc) { document.querySelector('.gd__desc').innerText = desc; }
-        });
-
-        target.append(detailButton);
-    });
-
-    item.addEventListener('mouseleave', function(e) {
-        var target = e.target;
-        var testButton = target.querySelector('.gl__test');
-        var detailButton = target.querySelector('.gl__detail');
-
-        detailButton.remove();
-    });
-});
-
-document.querySelector('.-alltest').addEventListener('click', function(e) {
-    document.querySelector('body').classList.toggle('test-mode');
-    this.classList.toggle('on');
-    var target = e.target;
-    var kk = target.querySelector('span');
-
-    if (kk.innerText === '테스트모드') {
-        kk.innerText = '카피모드';
-    } else {
-        kk.innerText = '테스트모드';
-    }
-});
 
 document.addEventListener('click', function(e) {
     if (!e.target.classList.contains('gd__close')) return;
@@ -183,20 +105,6 @@ document.querySelectorAll('.gn__group > button').forEach(function(button) {
 var openTimer;
 var closeTimer;
 
-/* document.querySelector('.gn').addEventListener('mouseenter', function(e) {
-    clearTimeout(closeTimer);
-    navTimer = setTimeout(function() {
-        document.querySelector('.gn').classList.add('-active');
-    }, 400);
-}); */
-
-/* document.querySelector('.gn').addEventListener('mouseleave', function(e) {
-    clearTimeout(openTimer);
-    closeTimer = setTimeout(function() {
-        document.querySelector('.gn').classList.remove('-active');
-    }, 1000);
-}); */
-
 window.addEventListener('resize', resize);
 
 function resize() {
@@ -208,23 +116,24 @@ function resize() {
 // resize();
 
 /* DARK MODE */
+const darkBtn = document.querySelector('.gh__util.-dark');
+if (darkBtn) {
+    darkBtn.addEventListener('click', function(e) {
+        console.log('dark')
+        var target = e.target;
+        var body = document.querySelector('body');
+        var keyword = target.querySelector('span');
 
+        body.classList.toggle('dark-mode');
+        target.classList.toggle('-active');
 
-document.querySelector('.gh__util.-dark').addEventListener('click', function(e) {
-    console.log('dark')
-    var target = e.target;
-    var body = document.querySelector('body');
-    var keyword = target.querySelector('span');
-
-    body.classList.toggle('dark-mode');
-    target.classList.toggle('-active');
-
-    if (keyword.innerText === '다크모드') {
-        keyword.innerText = '라이트모드';
-    } else {
-        keyword.innerText = '다크모드';
-    }
-});
+        if (keyword.innerText === '다크모드') {
+            keyword.innerText = '라이트모드';
+        } else {
+            keyword.innerText = '다크모드';
+        }
+    });
+}
 /* 
 $(document).ready(function(){
     if(document.querySelector('body').classList.contains('dark-mode')){
